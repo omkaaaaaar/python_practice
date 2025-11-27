79c01223e97b2d98cff378d1409292086360e440
omkaaaaaar <omkarpatkarr@gmail.com>
2025-11-27 12:02:08 +0530

    Update greet.py

--- greet.py
+++ greet.py
@@ -1,5 +1,5 @@
 def greet(name: str) -> None:
-  print(f"Hello,{name}!")
+  print(f"Hello,{name}!")  #ts f tells python that there is a variable inside {}, and w/o f it will print Hello, {name}!
 
 greet('World')
 greet('Omkar')
